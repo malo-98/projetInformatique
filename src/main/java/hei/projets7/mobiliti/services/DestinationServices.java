1645d6746dec922f9ac25807edf520599c2c1951
Malo Reinert <malo.reinert@hei.yncrea.fr>
2019-11-14 18:29:43 +0100

    liaison bdd avec list.html

--- src/main/java/hei/projets7/mobiliti/services/DestinationServices.java
+++ src/main/java/hei/projets7/mobiliti/services/DestinationServices.java
@@ -1,4 +1,25 @@
 package hei.projets7.mobiliti.services;
 
+import hei.projets7.mobiliti.daos.impl.DestinationDaoImpl;
+import hei.projets7.mobiliti.pojos.Destination;
+
+import java.util.List;
+
 public class DestinationServices {
+    private static  class  DestinationListHolder{
+        private final static DestinationServices instance = new DestinationServices();
+    }
+
+    public static DestinationServices getInstance(){return DestinationListHolder.instance;}
+
+    private DestinationDaoImpl destinationDao= new DestinationDaoImpl();
+
+    private DestinationServices(){
+    }
+
+    public List<Destination> destinationList(){return destinationDao.listDestinations();}
+
+    public Destination addDestination(Destination destination){return destinationDao.addDestination(destination);}
+
 }
+
