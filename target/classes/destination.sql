17df22c3e99159b9c0af61d2e1af9a5a6ec3a5b6
Paul Jourdan <Paul.jourdan-du-mazot-le-rat-de-magnitot@hei.yncrea.fr>
2019-11-18 09:40:25 +0100

    rangement doc CSS + changement structure html

--- target/classes/destination.sql
+++ target/classes/destination.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  mar. 12 nov. 2019 à 12:06
+-- Généré le :  lun. 18 nov. 2019 à 08:47
 -- Version du serveur :  10.4.6-MariaDB
 -- Version de PHP :  7.3.9
 
@@ -33,6 +33,8 @@ CREATE TABLE `destination` (
   `Nom` varchar(100) NOT NULL,
   `Ville` varchar(100) NOT NULL,
   `Pays` varchar(100) NOT NULL,
+  `Description` varchar(500) NOT NULL,
+  `Domaine` varchar(100) NOT NULL,
   `Nombre_de_place` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -40,9 +42,10 @@ CREATE TABLE `destination` (
 -- Déchargement des données de la table `destination`
 --
 
-INSERT INTO `destination` (`id_destination`, `Nom`, `Ville`, `Pays`, `Nombre_de_place`) VALUES
-(1, 'DestinationTest', 'VillTest', 'PaysTest', 4),
-(2, 'DestinationTest2', 'VilleTest2', 'PaysTest2', 8);
+INSERT INTO `destination` (`id_destination`, `Nom`, `Ville`, `Pays`, `Description`, `Domaine`, `Nombre_de_place`) VALUES
+(1, 'testNom1', 'testVille1', 'testPays1', 'LA vies est belle', 'testDomaine1', 4),
+(2, 'testNom2', 'testVille2', 'testPays2', 'test description 2', 'testDomaine2', 11),
+(11, 'Univ du champs', 'Paris', 'France', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\r\n                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 'ITI', 4);
 
 --
 -- Index pour les tables déchargées
@@ -62,7 +65,7 @@ ALTER TABLE `destination`
 -- AUTO_INCREMENT pour la table `destination`
 --
 ALTER TABLE `destination`
-  MODIFY `id_destination` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_destination` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
