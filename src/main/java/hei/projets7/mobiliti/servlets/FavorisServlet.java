47306f5543d34247877b0eb85b5c53be99e0a694
Malo Reinert <malo.reinert@hei.yncrea.fr>
2019-11-23 17:54:29 +0100

    update servlet

--- src/main/java/hei/projets7/mobiliti/servlets/FavorisServlet.java
+++ src/main/java/hei/projets7/mobiliti/servlets/FavorisServlet.java
@@ -1,5 +1,10 @@
 package hei.projets7.mobiliti.servlets;
 
+import hei.projets7.mobiliti.exception.EleveNotFoundException;
+import hei.projets7.mobiliti.pojos.Eleve;
+import hei.projets7.mobiliti.services.EleveServices;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -12,9 +17,22 @@ import java.io.IOException;
 @WebServlet("/favoris")
 public class FavorisServlet extends UtilsServlet {
 
+    private static final Logger LOGGER = LogManager.getLogger();
+    private EleveServices eleveServices = new EleveServices();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String utilisateurConnecte=(String) req.getSession().getAttribute("utilisateurConnecte");
         WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        Eleve eleve= null;
+        try {
+            eleve = eleveServices.getInstance().getEleve(utilisateurConnecte);
+        } catch (EleveNotFoundException e) {
+            e.printStackTrace();
+        }
+        context.setVariable("eleveConnecte",eleve);
+
         TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
         templateEngine.process("prive/Favoris", context, resp.getWriter());
     }
