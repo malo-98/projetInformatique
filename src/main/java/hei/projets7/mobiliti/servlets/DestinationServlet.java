353f88eef320becfee2ab5c37e77fd8c9ad1c0c9
Henri Gerardin <henri.gerardinphei.yncrea.fr>
2019-11-29 11:40:21 +0100

    modif destination

--- src/main/java/hei/projets7/mobiliti/servlets/DestinationServlet.java
+++ src/main/java/hei/projets7/mobiliti/servlets/DestinationServlet.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import hei.projets7.mobiliti.exception.DestinationNotFoundException;
 import hei.projets7.mobiliti.pojos.Destination;
 import hei.projets7.mobiliti.services.DestinationServices;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -16,8 +18,11 @@ import java.io.IOException;
 @WebServlet("/destination")
 public class DestinationServlet extends UtilsServlet {
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOGGER.info("destination detail" + req.getParameter("id"));
         int destinationId = Integer.parseInt(req.getParameter("id"));
 
         TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
@@ -26,7 +31,7 @@ public class DestinationServlet extends UtilsServlet {
         try {
             webContext.setVariable("destination", DestinationServices.getInstance().getDestination(destinationId));
         } catch (DestinationNotFoundException e) {
-            e.printStackTrace();
+            LOGGER.warn(e);
         }
 
         templateEngine.process("Destination", webContext, resp.getWriter());
