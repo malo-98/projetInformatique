557018fd7690a64a12b26849743dc0f1b7266721
Henri Gerardin <henri.gerardinphei.yncrea.fr>
2019-12-06 15:58:06 +0100

    ajax

--- src/main/java/hei/projets7/mobiliti/controllers/eleveController.java
+++ src/main/java/hei/projets7/mobiliti/controllers/eleveController.java
@@ -1,14 +1,22 @@
 package hei.projets7.mobiliti.controllers;
 
 import hei.projets7.mobiliti.exception.EleveNotFoundException;
+import hei.projets7.mobiliti.pojos.Eleve;
 import hei.projets7.mobiliti.services.EleveServices;
 
-import javax.ws.rs.DELETE;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/listEleve")
 public class eleveController {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Eleve> listEleve(){
+        return EleveServices.getInstance().listEleve();
+    }
+
     @DELETE
     @Path("/{eleveEmail}")
     public void deleteCity(
--- src/main/webapp/js/eleve.js
+++ src/main/webapp/js/eleve.js
@@ -1,9 +1,26 @@
+let listEleve = function () {
+    let elevesRequest = new XMLHttpRequest();
+    let url = "ws/listEleve";
+
+    elevesRequest.open("GET", url, true);
+    elevesRequest.responseType = "json";
+
+    elevesRequest.onload = function () {
+        let eleves = this.response;
+
+    };
+
+    elevesRequest.send();
+};
+
 let deleteEleve = function (eleve) {
     if (confirm("Are you sure you want to delete " + eleve.nom + " (" + eleve.id + ") ?")) {
         let deleteRequest = new XMLHttpRequest();
         deleteRequest.open("DELETE", "ws/listEleve/" + eleve.email, true);
         window.onload= function(){
-
+            let  request= new XMLHttpRequest();
+            request.open(GET, "/listEleve",true,null);
+            request.send();
         }
         deleteRequest.send();
     }
